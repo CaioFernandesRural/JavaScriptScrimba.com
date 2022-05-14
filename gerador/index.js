1f126c7d2a14d15dadcad63a572c2495e595a9e3
Caio <caioorebeo@gmail.com>
2022-05-14 20:21:14 -0300

    gerador completo

---
+++ gerador/index.js
@@ -0,0 +1,22 @@
+let senha1El = document.getElementById("senha1")
+let senha2El = document.getElementById("senha2")
+let senha3El = document.getElementById("senha3")
+let senha4El = document.getElementById("senha4")
+
+let possiveis = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']
+
+function generate() {
+    let senha = ''
+    for (let i = 0; i < 10; i++) {
+        senha += possiveis[Math.floor(Math.random() * possiveis.length)]
+    }
+    console.log(senha)
+    return senha
+}
+
+function display() {
+    senha1El.textContent = generate()
+    senha2El.textContent = generate()
+    senha3El.textContent = generate()
+    senha4El.textContent = generate()
+}

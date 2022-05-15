17ea20e267ec48332d65fae5f0ccf77b33888d05
Caio <caioorebeo@gmail.com>
2022-05-14 21:44:18 -0300

    tamanho variavel

--- gerador/index.js
+++ gerador/index.js
@@ -3,14 +3,17 @@ let senha2El = document.getElementById("senha2")
 let senha3El = document.getElementById("senha3")
 let senha4El = document.getElementById("senha4")
 
-let possiveis = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']
+let possiveis = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','!','@','#','$','%','&','*','+','=','?']
 
 function generate() {
     let senha = ''
-    for (let i = 0; i < 10; i++) {
+    let tamanho = document.getElementById("tamanho").value
+
+    for (let i = 0; i < tamanho; i++) {
         senha += possiveis[Math.floor(Math.random() * possiveis.length)]
     }
     console.log(senha)
+    console.log(tamanho)
     return senha
 }
 
